1759b39c7e68aeed1ab3da7c0459800cd63f3fd5
Hiwoshu <hiwoshu@hotmail.com>
2019-03-28 19:03:24 -0300

    Add components/navbartop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import NavbarTop from 'components/navbartop'
+import { Alert } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <NavbarTop />
+        <Alert color="primary">
+          This is a primary alert — check it out!
+        </Alert>
       </div>
     );
   }
